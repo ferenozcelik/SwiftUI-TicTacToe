bf8ee0573859e073d8a008ecb62f2debd3832368
Fatih Eren Özçelik <f.eren.ozcelik@gmail.com>
2024-03-20 20:48:35 +0300

    initial commit

---
+++ TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Fatih Eren Ozcelik on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
