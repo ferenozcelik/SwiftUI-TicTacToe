f2e404cff5bf28cd1b1a041e3eb41b7aa2eb8c3a
Fatih Eren Özçelik <f.eren.ozcelik@gmail.com>
2024-03-21 10:25:57 +0300

    Game board UI, Move object & occpied square check

--- ContentView.swift
+++ ContentView.swift
@@ -7,18 +7,70 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) {i in
+                        ZStack {
+                            Circle()
+                                .foregroundStyle(.blue).opacity(1)
+                                .frame(width: geometry.size.width / 3 - 15,
+                                       height: geometry.size.width / 3 - 15)
+                            
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundStyle(.white)
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) { return }
+                            moves[i] = Move(
+                                player: isHumansTurn ? .human : .computer,
+                                boardIndex: i
+                            )
+                            isHumansTurn.toggle()
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index })
     }
 }
 
 #Preview {
     ContentView()
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
+    }
 }
